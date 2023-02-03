ef8686b3d40cfbe6c7ce7d5333718b34dc1bc4f6
sweetpotatoX <abdobahaa53@gmail.com>
2023-02-03 14:03:30 +0200

    my js training

---
+++ first.js
@@ -0,0 +1,14 @@
+/* data types:
+undefined, null, boolean, string, symbol, number, object
+*/
+var myName = "sweet potato"
+myName = 8
+let ourname = "a7a lool"
+const pi = 3.14
+console.log(pi)
+var a;
+var b = 2;
+console.log(a)
+a = 7
+b = a;
+console.log(a)
