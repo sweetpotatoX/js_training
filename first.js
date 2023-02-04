3ec4989f0c8380036cfe3a0baa8e067bacd0a3fb
sweetpotatoX <abdobahaa53@gmail.com>
2023-02-05 01:32:53 +0200

    calculator

--- first.js
+++ first.js
@@ -1,14 +1,98 @@
-/* data types:
-undefined, null, boolean, string, symbol, number, object
-*/
-var myName = "sweet potato"
-myName = 8
-let ourname = "a7a lool"
-const pi = 3.14
-console.log(pi)
-var a;
-var b = 2;
-console.log(a)
-a = 7
-b = a;
-console.log(a)
+let runningtotal = 0;
+let buffer = "0";
+let previousoperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonclick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handlenumber(value);
+    }
+    screen.innerText = buffer;
+
+}
+
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'C':
+            buffer = '0';
+            runningtotal = 0;
+            break;
+        case '=':
+            if (previousoperator === null) {
+                return
+            }
+            flushoperation(parseInt(buffer));
+            previousoperator = null;
+            buffer = runningtotal;
+            runningtotal = 0;
+            break;
+        case '←':
+            if (buffer.length === 1) {
+                buffer = '0';
+            } else {
+                buffer = buffer.substring(0, buffer.length - 1);
+
+            }
+            break;
+        case '+':
+        case '−':
+        case '×':
+        case '÷':
+            handlemath(symbol);
+            break;
+    }
+
+}
+
+function handlemath(symbol) {
+    if (buffer === '0') {
+        return;
+    }
+
+    const intbuffer = parseInt(buffer);
+
+    if (runningtotal === 0) {
+        runningtotal = intbuffer;
+    } else {
+        flushoperation(intbuffer);
+    }
+    previousoperator = symbol;
+    buffer = '0';
+
+}
+
+function flushoperation(intbuffer) {
+    if (previousoperator === '+') {
+        runningtotal += intbuffer;
+    } else if (previousoperator === '−') {
+        runningtotal -= intbuffer;
+    } else if (previousoperator === '×') {
+        runningtotal *= intbuffer;
+    } else if (previousoperator === '÷') {
+        runningtotal /= intbuffer;
+    }
+
+}
+
+function handlenumber(numberstring) {
+    if (buffer === '0') {
+        buffer = numberstring;
+
+    } else {
+        buffer += numberstring;
+    }
+
+}
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function(event) {
+        buttonclick(event.target.innerText);
+    })
+
+
+}
+
+init();
